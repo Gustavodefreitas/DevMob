58a5fa620b6649d1626e0cdeacdffadefaf4c2f2
Gustavo <ovatsug.gu@hotmail.com>
2022-05-11 03:48:39 -0300

    confirma

---
+++ src/pages/favorito/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function favorito() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>FAVORITO</Text>
+      <StatusBar style="auto" />
+    </View>
+    
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text:{
+    fontSize:25,
+    fontWeight:'bold',
+  }
+});
