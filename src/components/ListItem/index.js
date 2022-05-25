769182d6ba9027df1d821512e98503a5bad5f62e
Gustavo <ovatsug.gu@hotmail.com>
2022-05-24 22:33:17 -0300

    ultima att dia 24/06

--- src/components/ListItem/index.js
+++ src/components/ListItem/index.js
@@ -1,22 +1,30 @@
 import React from "react";
-import { StyleSheet, Text, View,TouchableOpacity } from 'react-native';
+import { StyleSheet, Text,Image,View,TouchableOpacity } from 'react-native';
 import { AntDesign } from '@expo/vector-icons'; 
 import { trocaFav } from "../../services/tattoo";
 
 
 
-export default function ListItem({name,des,fav,id}){
+export default function ListItem({name,des,fav,id,img}){
     return(
         <View style={styles.container}>
-            <Text style={styles.nametext}>
-                {name}               
-            </Text>
+            
+        <View>
+          <Image source={{ uri: img }} style={styles.img} />
+        </View>
 
-            <Text>
+        <View>
+
+          <Text style={styles.nametext}>
+            {name}
+          </Text>
+          <Text style={styles.des}>
             {des}
-            </Text>
-            <TouchableOpacity onPress={()=>trocaFav(id)}>
-                {fav ? <AntDesign name="heart" size={24} color="black" />: <AntDesign name="hearto" size={24} color="black" />}
+          </Text >
+        </View>
+
+            <TouchableOpacity onPress={()=>trocaFav(id)} style={styles.coracao}>
+                {fav ? <AntDesign name="heart" size={24} color="black" />: <AntDesign name="heart" size={24} color="red" />}
             </TouchableOpacity>
             
         </View>
@@ -25,16 +33,33 @@ export default function ListItem({name,des,fav,id}){
 }
 const styles = StyleSheet.create({
     container: {
-      backgroundColor:'red',
+      backgroundColor:'grey',
       marginBottom:10,
       width:'95%',
       height:150,
-      
+      marginLeft: 15,
+      borderRadius: 28,
+
       
     },
     nametext:{
+      marginLeft: 150,
       fontSize:25,
       fontWeight:'bold',
+    },
+    coracao:{
+        marginLeft: 500,
+        fontSize:25,
+        fontWeight:'bold',
+      },
+    des:{
+        marginLeft: 150,
+        marginTop: 10,
+        
+    },
+    img:{
+      width:80,
+      height:80,
     }
   });
   
--- src/pages/inicial/index.js
+++ src/pages/inicial/index.js
@@ -1,30 +1,106 @@
 import React from 'react';
-import { StyleSheet, Text, ScrollView} from 'react-native';
+import { StyleSheet, Text, FlatList, Image, TouchableOpacity, View} from 'react-native';
+import { AntDesign } from '@expo/vector-icons'; 
+import { useState } from 'react';
 import ListItem from '../../components/ListItem';
 import { tatto } from '../../services/tattoo';
+import { useNavigation } from '@react-navigation/native';
 
-export default function inicial() {
+
+
+export default function Inicial() {
+  const [tt, setTt] = useState(tatto)
+  const navigation = useNavigation();
+
+  const onHandleClick = (item) => {
+    navigation.navigate('Favorito', {item})
+  }
+  const exibeLista = ({item}) => {
+    return (
+
+      <View style={styles.container}>
+
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: item.img }} style={styles.img} />
+        </View>
+
+        <View style={styles.conteudo}>
+
+          <Text style={styles.nametext}>
+            {item.name}
+          </Text>
+          <Text style={styles.des}>
+            {item.des}
+          </Text >
+        </View>
+
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity onPress={() => onHandleClick(item)} style={styles.coracao}>
+            {item.fav ? <AntDesign name="heart" size={24} color="black" /> : <AntDesign name="heart" size={24} color="red" />}
+          </TouchableOpacity>
+        </View>
+
+      </View>
+  
+    )
+  }
+
   return (
-    <ScrollView style={styles.container}>
-      {
-        tatto.map(t=>(<ListItem key={t.id} name={t.name} fav={t.fav} id={t.id}>
-
-        </ListItem>))
-      }
-
-    </ScrollView>
+    <>
+      <FlatList
+        data={tt}
+        renderItem={exibeLista} 
+      />
+   </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#00ffff',
+    backgroundColor:'grey',
+    marginBottom:10,
+    width:'95%',
+    height:150,
+    marginLeft: 15,
+    borderRadius: 28,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
     
-   
   },
-  text:{
+  imageContainer: {
+    flex: 1,
+    width: 80,
+    height: 80,
+    padding: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  conteudo: {
+    justifyContent: 'center',
+    flex: 2,
+    padding: 10
+  },
+  buttonContainer: {
+    flex: 0.5,
+  },
+  nametext:{
+    marginLeft: 10,
     fontSize:25,
     fontWeight:'bold',
+  },
+  coracao:{
+      marginLeft: 10,
+      fontSize:25,
+      fontWeight:'bold',
+    },
+  des:{
+      marginLeft: 10,
+      marginTop: 10,
+      
+  },
+  img:{
+    width:80,
+    height:80,
   }
 });
