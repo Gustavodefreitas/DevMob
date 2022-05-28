203d894405b765eac63512a41db1d07ce375395e
DerikCaceres <derik.maganhato@hotmail.com>
2022-05-28 12:48:56 -0300

    commit fav

--- src/pages/favorito/index.js
+++ src/pages/favorito/index.js
@@ -1,29 +1,62 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
-export default function Favorito({route}) {
-  // const [favorites, setFavorites] = useState([])
-
+export default function Favorito({ route }) {
+  const [favTt, setFavTt] = useState([]) //variavel de estado que cria um novo array para favoritos
   const info = route.params.item;
-  console.log(info)
+
+  useEffect(() => { //Hook que executa sempre que a tela abre
+    addOnFavs(info)
+  }, [info])
+
+
+  function addOnFavs(info) { //adiciona ao array favoritos
+    if (info.fav == false) {
+      const index = favTt.indexOf(info)
+      favTt.splice(index, 1)
+      info = null
+
+    } else {
+      favTt.push(info)
+      info = null
+    }
+  }
+
+  const renderFav = ({ item }) => {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>{item.name}</Text>
+        <Text style={styles.text}>{item.des}</Text>
+      </View>
+    )
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>{info.name}</Text>
-      <Text style={styles.text}>{info.des}</Text>
-    </View>
-    
+    <FlatList
+      data={favTt}
+      renderItem={renderFav}
+    />
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#d8d8d8'
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
+    marginBottom: 10,
+    width: '95%',
+    marginLeft: '2.5%',
+    height: 120,
+    borderRadius: 28,
+    flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
+    elevation: 10
   },
-  text:{
-    fontSize:25,
-    fontWeight:'bold',
+  text: {
+    fontSize: 25,
+    fontWeight: 'bold',
   }
 });
--- src/pages/inicial/index.js
+++ src/pages/inicial/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, FlatList, Image, TouchableOpacity, View} from 'react-native';
-import { AntDesign } from '@expo/vector-icons'; 
+import { StyleSheet, Text, FlatList, Image, TouchableOpacity, View } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 import { useState } from 'react';
 import ListItem from '../../components/ListItem';
-import { tatto } from '../../services/tattoo';
+import { tatto, favTattoo } from '../../services/tattoo';
 import { useNavigation } from '@react-navigation/native';
 
 
 
 export default function Inicial() {
-  const [tt, setTt] = useState(tatto)
+  const [tt, setTt] = useState(tatto) //variavel de estado, que recebe o service Tattoo
   const navigation = useNavigation();
 
-  const onHandleClick = (item) => {
-    navigation.navigate('Favorito', {item})
+
+  function onCheckboxChanged(index1) { //executa essa função sempre que clica no coração
+    const newTt = [...tt];
+    newTt[index1].fav = !newTt[index1].fav;
+    setTt(newTt);
   }
-  const exibeLista = ({item}) => {
+
+  function buscaNoBanco() { //função para buscar em um banco futuro
+    //Faz o get: bancoMongo.com/buscaTattoo => Array[]
+    // setTt(Array[])
+  }
+
+
+  const onHandleClick = (item, index) => {
+    onCheckboxChanged(index);
+    navigation.navigate('Favorito', { item })
+  }
+  const exibeLista = ({ item, index }) => {
     return (
 
       <View style={styles.container}>
@@ -35,38 +49,42 @@ export default function Inicial() {
         </View>
 
         <View style={styles.buttonContainer}>
-          <TouchableOpacity onPress={() => onHandleClick(item)} style={styles.coracao}>
-            {item.fav ? <AntDesign name="heart" size={24} color="black" /> : <AntDesign name="heart" size={24} color="red" />}
+          <TouchableOpacity onPress={() => onHandleClick(item, index)} style={styles.coracao}>
+            {item.fav ? <AntDesign name="heart" size={24} color="red" /> : <AntDesign name="heart" size={24} color="black" />}
           </TouchableOpacity>
         </View>
 
       </View>
-  
+
     )
   }
 
   return (
-    <>
+    <View style={styles.main}>
       <FlatList
         data={tt}
-        renderItem={exibeLista} 
+        renderItem={exibeLista}
+        contentContainerStyle={{ padding: 10 }}
       />
-   </>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    backgroundColor: '#d8d8d8',
+  },
   container: {
-    backgroundColor:'grey',
-    marginBottom:10,
-    width:'95%',
-    height:150,
-    marginLeft: 15,
+    backgroundColor: 'white',
+    marginBottom: 10,
+    width: '95%',
+    marginLeft: '2.5%',
+    height: 120,
     borderRadius: 28,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
-    
+    elevation: 10
   },
   imageContainer: {
     flex: 1,
@@ -84,23 +102,23 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flex: 0.5,
   },
-  nametext:{
+  nametext: {
     marginLeft: 10,
-    fontSize:25,
-    fontWeight:'bold',
+    fontSize: 25,
+    fontWeight: 'bold',
   },
-  coracao:{
-      marginLeft: 10,
-      fontSize:25,
-      fontWeight:'bold',
-    },
-  des:{
-      marginLeft: 10,
-      marginTop: 10,
-      
+  coracao: {
+    marginLeft: 10,
+    fontSize: 25,
+    fontWeight: 'bold',
   },
-  img:{
-    width:80,
-    height:80,
+  des: {
+    marginLeft: 10,
+    marginTop: 5,
+
+  },
+  img: {
+    width: 80,
+    height: 80,
   }
 });
